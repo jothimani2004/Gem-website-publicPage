e08ea9ea241be4a33c7068cfe55a40f5e8062231
jothimani2004 <jothimani88531@gmail.com>
2026-05-01 13:48:08 +0530

    final

--- src/Components/common/Loader/Loader.jsx
+++ src/Components/common/Loader/Loader.jsx
@@ -3,24 +3,33 @@ import styles from "./Loader.module.css";
 function Loader() {
   return (
     <div className={styles.container}>
-      
-      {/* ✨ PARTICLES */}
-      <div className={styles.particles}></div>
-
-      {/* 💎 GEM */}
       <div className={styles.gemWrapper}>
-        <div className={styles.gem}>
-          <div className={styles.face}></div>
-          <div className={styles.face}></div>
-          <div className={styles.face}></div>
-          <div className={styles.face}></div>
-        </div>
+        <svg
+          viewBox="0 0 100 100"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={styles.diamond}
+        >
+          {/* Main Diamond Outline */}
+          <path
+            d="M25 35 L50 15 L75 35 L50 85 L25 35 Z"
+            className={styles.outline}
+          />
+          {/* Internal Facet Lines */}
+          <path d="M25 35 L75 35" className={styles.line} />
+          <path d="M50 15 L50 85" className={styles.line} />
+          <path d="M50 35 L25 35" className={styles.line} />
+          <path d="M50 35 L75 35" className={styles.line} />
+          <path d="M50 35 L50 15" className={styles.line} />
+        </svg>
+        <div className={styles.sparkle}></div>
       </div>
 
-      {/* TEXT */}
-      <h2 className={styles.title}>AIMPLUSS</h2>
-      <p className={styles.subtitle}>CRAFTING BRILLIANCE</p>
-
+      <div className={styles.textContainer}>
+        <h2 className={styles.title}>AIMPLUSS</h2>
+        <div className={styles.loaderBar}></div>
+        <p className={styles.subtitle}>CRAFTING BRILLIANCE</p>
+      </div>
     </div>
   );
 }
